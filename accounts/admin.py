77fe13bd3d26a02ad185a2fa4030fe871d842bf0
John Caporale <john@coffeepls.dev>
2022-10-09 22:49:38 -0400

    Revert "Revert "Initial Private Message Implementation""
    This reverts commit 7c06bb8d71c153712383512aa6a78ff1fedd9e52.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import PrivateMessage, PrivateMessageReplies
+
+admin.site.register(PrivateMessage)
+admin.site.register(PrivateMessageReplies)
