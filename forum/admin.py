03b67c0a9da94db235f9f4a3755581d4ea149c74
John Caporale <john@coffeepls.dev>
2022-10-15 21:07:30 -0400

    Add migrations to git

--- forum/admin.py
+++ forum/admin.py
@@ -5,17 +5,27 @@ from django.contrib import admin
 from .models import Topic, Post, Comment
 
 
+# Comment inline class
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
 # Register Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('post_topic', 'post_title', 'post_user', 'created_at', 'post_locked', 'post_visible', 'post_sticky')
+    list_display = (
+        'post_topic', 'post_title', 'post_user', 'created_at', 'updated_at', 'post_locked', 'post_visible',
+        'post_sticky')
     list_filter = ('created_at', 'post_user', 'post_topic')
     search_fields = ('post_title', 'post_content')
-    date_hierarchy = 'created_at'
+    date_hierarchy = 'updated_at'
+
+    # Display comments inline
+    inlines = [
+        CommentInline,
+    ]
+
 
 admin.site.register(Topic)
-
-admin.site.register(Comment)
-
 
 
